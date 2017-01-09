5c9d9a21d74065e51c80725b89585d1f4cc8523e
cpoetter <cpoetter@stanford.edu>
2017-01-09 21:49:29 +0000

    Improve saving.

--- faceDetection.py
+++ faceDetection.py
@@ -44,51 +44,55 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         flags=cv2.cv.CV_HAAR_SCALE_IMAGE
     )
 
-    # If faces are found, welcome the them
+    # If new faces are found, welcome them, and save resized face image for training
     if len(faces) > previous_faces:
-       welcome_text = 'Willkommen bei Elgato!'
-       print welcome_text
-       # os.system("./speech.sh " + welcome_text + " &")
-       timestamp = str(int(time.time()))
-       cv2.imwrite("training/face_" + timestamp + "_" + ".jpg", image)
+        welcome_text = 'Willkommen bei Elgato!'
+        print welcome_text
+        # os.system("./speech.sh " + welcome_text + " &")
+        timestamp = str(int(time.time()))
+        cv2.imwrite("training/face_" + timestamp + "_" + ".jpg", image)
+
+        face_counter = 0
+        for (x, y, w, h) in faces:
+
+            # All faces must have the same size at the end, make the face area wider or higher
+            # until they fit the target size
+            target_face_width = 120
+            target_face_height = 150
+            target_face_ratio = float(target_face_width)/float(target_face_height)
+            face_ratio = float(w)/float(h)
+
+            if target_face_ratio < face_ratio:
+                new_face_width = w
+                new_face_height = int(w / target_face_ratio)
+                shift = int(float(new_face_height - h)/2)
+                y = y - shift
+                h = h + 2*shift
+            elif target_face_ratio > face_ratio:
+                new_face_height = h
+                new_face_width = int(h * target_face_ratio)
+                shift = int(float(new_face_width - w)/2)
+                x = x - shift
+                w = w + 2*shift
+
+            # Save images in gray, not RGB
+            face_gray = gray[y:(y+h+1), x:(x+w+1)]
+            final_face_gray = cv2.resize(face_gray, (target_face_width, target_face_height), interpolation = cv2.INTER_CUBIC)
+
+            # If in debug mode, display result
+            if debug_mode == 'debug':
+                cv2.imshow(str(face_counter), final_face_gray)
+
+            # save result
+            cv2.imwrite("training/face_" + timestamp + "_" + str(face_counter) + ".jpg", final_face_gray)
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            face_counter += 1
+
     previous_faces = len(faces)
 
     # Draw a rectangle around the faces
-    face_counter = 0
     for (x, y, w, h) in faces:
-
-        # All faces must have the same size at the end, make the face area wider or higher 
-        # until they fit the target size
-        target_face_width = 120
-        target_face_height = 150
-        target_face_ratio = float(target_face_width)/float(target_face_height)
-        face_ratio = float(w)/float(h)
-
-        if target_face_ratio < face_ratio:
-            new_face_width = w
-            new_face_height = int(w / target_face_ratio)
-            shift = int(float(new_face_height - h)/2)
-            y = y - shift
-            h = h + 2*shift
-        elif target_face_ratio > face_ratio:
-            new_face_height = h
-            new_face_width = int(h * target_face_ratio)
-            shift = int(float(new_face_width - w)/2)
-            x = x - shift
-            w = w + 2*shift
-
-        # Save images in gray, not RGB
-        face_gray = gray[y:(y+h+1), x:(x+w+1)]
-        final_face_gray = cv2.resize(face_gray, (target_face_width, target_face_height), interpolation = cv2.INTER_CUBIC)
-
-        # If in debug mode, display result
-        if debug_mode == 'debug':
-            cv2.imshow(str(face_counter), final_face_gray)
-
-        # save result
-        cv2.imwrite("training/face_" + timestamp + "_" + str(face_counter) + ".jpg", final_face_gray)
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        face_counter += 1
 
     # Display the resulting frame
     if debug_mode == 'debug':
